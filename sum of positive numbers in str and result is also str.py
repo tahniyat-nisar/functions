08b121dbf27db4324fdd3f965b60a92e69363e49
tahniyat-nisar <93429497+tahniyat-nisar@users.noreply.github.com>
2021-12-04 16:51:53 +0530

    Create sum of positive numbers in str and result is also str.py

---
+++ sum of positive numbers in str and result is also str.py
@@ -0,0 +1,8 @@
+# "4", "5" --> "9"
+# "34", "5" --> "39"
+def sumofstrs():
+    a=input("enter a number:")
+    b=input("enter a number:")
+    sum=int(a)+int(b)
+    return (str(sum))
+print(sumofstrs())
